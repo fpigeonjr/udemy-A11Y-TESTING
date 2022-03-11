2c135d03f88779b2370a6cd4e270136da3fcfb86
Frank Pigeon <frank.pigeonjr@gmail.com>
2022-03-10 22:13:39 -0600

    adds cypress

---
+++ initial-code/chakra-ui/cypress/plugins/index.js
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+const { lighthouse, prepareAudit } = require("@cypress-audit/lighthouse")
+
+module.exports = (on, config) => {
+  on("before:browser:launch", (browser = {}, launchOptions) => {
+    prepareAudit(launchOptions)
+  })
+
+  on("task", {
+    lighthouse: lighthouse() // calling the function is important
+  })
+}
